f9ea5540394902ef84f6a11d7a3adb1d0fbf234c
Lucas <lucassimoes_@hotmail.com>
2023-05-22 06:14:44 -0300

    update

--- ssm/managed-instance-setup.sh
+++ ssm/managed-instance-setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-mkdir /tmp/ssm
-curl https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm -o /tmp/ssm/amazon-ssm-agent.rpm
-sudo yum install -y /tmp/ssm/amazon-ssm-agent.rpm
-sudo systemctl stop amazon-ssm-agent
+# if you need to install the SSM Agent manually:
+sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+
+# check status ubuntu
+sudo systemctl status snap.amazon-ssm-agent.amazon-ssm-agent.service
+sudo snap stop amazon-ssm-agent
 # edit the code, id and region in the command below
-sudo amazon-ssm-agent -register -code "activation-code" -id "activation-id" -region "region"
-sudo systemctl start amazon-ssm-agent
+sudo /snap/amazon-ssm-agent/current/amazon-ssm-agent -register -code "activation-code" -id "activation-id" -region "region" 
+sudo snap start amazon-ssm-agent
